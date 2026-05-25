3c17552eb39a582c4f07b81302c1cfcb2a3542ae
Srivathsav Kyatham <2023.srivathsavk@isu.ac.in>
2026-05-25 15:24:06 +0530

    add all files

---
+++ selenum_demo/src/main/java/pages/LoginTestValid.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTestValid {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Launch Chrome
+        WebDriver driver = new ChromeDriver();
+
+        // Open login page
+        driver.get("http://localhost:8090/login");
+
+        // Maximize window
+        driver.manage().window().maximize();
+
+        // Wait for page to load
+        Thread.sleep(2000);
+
+        // Find email field and enter email
+        WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("hello@hello.com");
+
+        // Find password field and enter password
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("hello@hello.com");
+
+        // Click Sign In button
+        WebElement loginButton = driver.findElement(By.className("login-button"));
+        loginButton.click();
+
+        // Wait to observe result
+        Thread.sleep(3000);
+
+        // Close browser
+        driver.quit();
+    }
+}
